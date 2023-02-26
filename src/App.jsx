bf1d17cbfe0afcbfb81901d5d2d66db839471862
Veljko <veljkoBitno@gmail.com>
2023-02-26 19:01:19 +0100

    Styling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.scss";
+import Logo from "../images/logo.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <header>
+        <h1>
+          <img src={Logo} alt="Splitter logo" />
+        </h1>
+      </header>
+      <main>
+        <form>
+          <div className="controls">
+            <h2 className="bill">Bill</h2>
+            <input className="bill-input" placeholder="0" type="number" />
+            <h2 className="tip">Select Tip %</h2>
+            <div className="tip-buttons">
+              <button className="tip-button">5%</button>
+              <button className="tip-button">10%</button>
+              <button className="tip-button">15%</button>
+              <button className="tip-button">25%</button>
+              <button className="tip-button">50%</button>
+              <button className="tip-button custom">Custom</button>
+            </div>
+            <h2 className="people">Number of People</h2>
+            <input className="people-input" placeholder="0" type="number" />
+          </div>
+          <div className="stats">
+            <div className="stats-container tip-amount">
+              <p className="stats-para">
+                <span className="bold">Tip Amount</span> / person
+              </p>
+              <p className="tip-amount-value">$0</p>
+            </div>
+            <div className="stats-container total">
+              <p className="stats-para">
+                <span className="bold">Total</span> / person
+              </p>
+              <p className="total-value">$0</p>
+            </div>
+            <button className="reset">Reset</button>
+          </div>
+        </form>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
