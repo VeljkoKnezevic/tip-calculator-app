ebe77bcd4fb4be054fad81f9728bf9bfc74ba55e
Veljko <veljkoBitno@gmail.com>
2023-02-27 12:52:42 +0100

    Progress on input functionallity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
 import "./App.scss";
 import Logo from "../images/logo.svg";
+import { useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [people, setPeople] = useState(0);
+
+  const handleButton = (e) => {
+    e.preventDefault();
+    setTip(parseInt(e.target.value));
+    e.target.parentElement.lastChild.value = "";
+  };
+
+  const handleInputs = (e) => {
+    const input = e.target;
+    let inputValue = parseInt(input.value);
+
+    if (input.classList.contains("bill-input")) {
+      if (inputValue === 0) {
+        inputValue = "";
+      }
+      setBill(inputValue);
+    }
+
+    if (input.classList.contains("custom")) {
+      if (inputValue === 0) {
+        inputValue = "";
+      }
+      setTip(inputValue);
+    }
+
+    if (input.classList.contains("people-input")) {
+      let error = e.target.parentElement.children[4];
+      if (inputValue === 0) {
+        inputValue = "";
+        input.style.borderColor = "#E17457";
+        error.style.display = "block";
+        //Srediti
+      }
+      setPeople(inputValue);
+    }
+  };
+
   return (
     <>
       <header>
@@ -13,18 +54,48 @@ function App() {
         <form>
           <div className="controls">
             <h2 className="bill">Bill</h2>
-            <input className="bill-input" placeholder="0" type="number" />
+            <input
+              className="bill-input"
+              onChange={handleInputs}
+              value={bill === 0 ? "" : bill}
+              placeholder="0"
+              type="number"
+            />
             <h2 className="tip">Select Tip %</h2>
             <div className="tip-buttons">
-              <button className="tip-button">5%</button>
-              <button className="tip-button">10%</button>
-              <button className="tip-button">15%</button>
-              <button className="tip-button">25%</button>
-              <button className="tip-button">50%</button>
-              <button className="tip-button custom">Custom</button>
+              <button className="tip-button" value={5} onClick={handleButton}>
+                5%
+              </button>
+              <button className="tip-button" value={10} onClick={handleButton}>
+                10%
+              </button>
+              <button className="tip-button" value={15} onClick={handleButton}>
+                15%
+              </button>
+              <button className="tip-button" value={25} onClick={handleButton}>
+                25%
+              </button>
+              <button className="tip-button" value={50} onClick={handleButton}>
+                50%
+              </button>
+              <input
+                type="number"
+                placeholder="Custom"
+                onChange={handleInputs}
+                className="tip-button custom"
+              />
             </div>
-            <h2 className="people">Number of People</h2>
-            <input className="people-input" placeholder="0" type="number" />
+            <div className="h2-container">
+              <h2 className="people">Number of People</h2>
+              <div className="error">Can't be zero</div>
+            </div>
+            <input
+              onChange={handleInputs}
+              value={people === 0 ? "" : people}
+              className="people-input"
+              placeholder="0"
+              type="number"
+            />
           </div>
           <div className="stats">
             <div className="stats-container tip-amount">
